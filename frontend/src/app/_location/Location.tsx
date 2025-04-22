d66684bb68628c63570ef4ed032906d617b3b3fd
tvkeprof <wefousoul@gmail.com>
2025-04-22 18:34:58 +0800

    loc

--- frontend/src/app/_location/Location.tsx
+++ frontend/src/app/_location/Location.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Location = () => {
   const number = 1;
   const router = useRouter();
   return (
-    <div className="max-w-2xl w-full h-full bg-red-300">
-      <button onClick={() => router.push(`/location/${number}`)}>
-        Aguulah
-      </button>
+    <div className="max-w-2xl w-full h-full bg-[#e9ecef]">
+      <div className="p-4">
+        <Button
+          className="w-full h-12 bg-white text-black justify-between rounded-lg flex font-bold"
+          onClick={() => router.push(`/address/${number}`)}
+        >
+          Эрээн агуулах
+          <ChevronRight />
+        </Button>
+      </div>
     </div>
   );
 };
--- frontend/src/app/address/[number]/page.tsx
+++ frontend/src/app/address/[number]/page.tsx
@@ -1,10 +1,106 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  ChevronLeft,
+  ClipboardCopy,
+  Globe,
+  Mail,
+  MapPin,
+  Phone,
+  Tag,
+  User,
+} from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+const CopyField = ({
+  label,
+  text,
+  Icon,
+}: {
+  label: string;
+  text: string;
+  Icon: React.ElementType;
+}) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
+  return (
+    <div>
+      <p className="text-sm mb-1 flex items-center gap-2">
+        <Icon className="w-4 h-4" />
+        {label}
+      </p>
+      <div className="border bg-white rounded-sm p-3 flex items-center justify-between">
+        <span className="text-sm">{text}</span>
+        <button
+          onClick={handleCopy}
+          className="flex items-center space-x-1 text-blue-600 text-xs"
+        >
+          <ClipboardCopy className="w-4 h-4" />
+          <span>{copied ? "Хуулсан" : "Хуулах"}</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const DetailedAddress = () => {
   const { number } = useParams();
-  return <div>{number}</div>;
+  const router = useRouter();
+
+  return (
+    <div className="w-screen h-screen flex flex-col bg-[#dddddd] items-center justify-center">
+      <div className="max-w-2xl w-full h-full bg-[#e9ecef] px-6 py-4 flex flex-col gap-5">
+        <div className="flex items-center gap-4">
+          <Button className="bg-white p-2" onClick={() => router.back()}>
+            <ChevronLeft className="text-black" />
+          </Button>
+          <h1 className="text-xl font-bold">Эрээн</h1>
+        </div>
+
+        <div className="flex justify-between border bg-white p-4 rounded-sm text-center">
+          <div>
+            <p className="text-sm">1кг ачаа</p>
+            <p className="font-semibold">3000 ₮</p>
+          </div>
+          <div>
+            <p className="text-sm">1м3-с доош</p>
+            <p className="font-semibold">599 ¥ </p>
+          </div>
+          <div>
+            <p className="text-sm">1м3-с дээш</p>
+            <p className="font-semibold">399 ¥</p>
+          </div>
+        </div>
+        <div className="space-y-4">
+          <CopyField
+            label="收件人 (Хүлээн авагч)"
+            text="烸嵪 (Өөрийн утасны дугаар)"
+            Icon={User}
+          />
+          <CopyField label="电话 (Утас)" text="15847901990" Icon={Phone} />
+          <CopyField
+            label="所在地区 (Бүс нутаг)"
+            text="内蒙古自治区 锡林郭勒盟 二连浩特市社区建设管理局"
+            Icon={Globe}
+          />
+          <CopyField
+            label="街道地址 (Хаяг)"
+            text="浩特汇通物流园区C05号 (Өөрийн утасны дугаар)"
+            Icon={MapPin}
+          />
+          <CopyField label="邮编 (Зип код)" text="011100" Icon={Tag} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailedAddress;
