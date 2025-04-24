628ff0269e4ee25638217847596f78f0eea5f0b2
tvkeprof <wefousoul@gmail.com>
2025-04-24 19:19:10 +0800

    locBack

--- backend/src/router/location.router.ts
+++ backend/src/router/location.router.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { CreateLocationController } from "../controller/location/CreateLocation.controller";
 import { GetLocationController } from "../controller/location/GetLocation.controller";
+import { PutLocationController } from "../controller/location/PutLocation.controller";
 
 export const LocationRouter = express.Router();
 LocationRouter.post("/", CreateLocationController);
 LocationRouter.get("/", GetLocationController);
+LocationRouter.put("/", PutLocationController);
