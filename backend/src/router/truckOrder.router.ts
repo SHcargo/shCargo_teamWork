8e244efbd243c5d47ecd250d88fc3affc2ff3d9c
Vskroi <torb06221418@gmail.com>
2025-04-27 23:51:57 -0700

    cargo

--- backend/src/router/truckOrder.router.ts
+++ backend/src/router/truckOrder.router.ts
@@ -1,4 +1,9 @@
 import express from "express";
 import { TruckItemsController } from "../controller/truckItems/CreateTruckOrder.controller";
+import { GetTruckItemsController } from "../controller/truckItems/GetTruckItems.controller";
+import { GetUserSelectOneOrderItems } from "../controller/truckItems/GetUserSelectOneOrderItems.controller";
 export const TruckOrdersRouter = express.Router();
+
 TruckOrdersRouter.post("/", TruckItemsController);
+TruckOrdersRouter.get("/:userId", GetTruckItemsController)
+TruckOrdersRouter.get("/:userId/:id", GetUserSelectOneOrderItems)
