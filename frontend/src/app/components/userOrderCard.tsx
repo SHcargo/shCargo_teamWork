90ffed2779cbc9e47a3b2c97473e0a345d3242fb
Vskroi <torb06221418@gmail.com>
2025-05-06 00:53:38 -0700

    z

--- frontend/src/app/components/userOrderCard.tsx
+++ frontend/src/app/components/userOrderCard.tsx
@@ -1,41 +1,105 @@
 "use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import axios from "axios";
+import { Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+
+type UserOrderCardProps = {
+  description: string;
+  trackingNumber: string;
+  id: string;
+  activeCategory: string;
+  statusHistory: { status: string; changedAt: string }[];
+  ref: () => void;
+};
 
 export const UserOrderCard = ({
   description,
   id,
-  statusHistory,
   trackingNumber,
-}: {
-  description: string;
-  trackingNumber: string;
-  id: string;
-  statusHistory: { status: string; changedAt: string }[];
-}) => {
+  activeCategory,
+  ref,
+}: UserOrderCardProps) => {
   const router = useRouter();
 
+  const showTrash =
+    ["Бүртгэсэн", "Хаагдсан"].includes(activeCategory) ||
+    ["Бүртгэсэн", "Хаагдсан"].includes(description);
+
+  const DeleteTruckingByTrackingNumber = async (trackingNumber: string) => {
+    try {
+      await axios.delete(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/truckItems/${trackingNumber}`
+      );
+      ref();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="bg-white p-4 rounded-xl border border-gray-200 shadow-sm cursor-pointer mb-4">
-      <div
-        className="flex justify-between w-full "
-        onClick={() => router.push(`/goodsForUsers/${id}`)}
-      >
-        <h2 className="text-lg font-semibold flex items-center">📦 Ачаа</h2>
-        <div className="flex">
-          <p className="text-sm text-gray-500 mt-1">{description}</p>
-          <button className="text-gray-400 text-xl">›</button>
-          <div>delete</div>
+    <div className="bg-white p-2 rounded-xl border border-gray-200 shadow-sm cursor-pointer mb-4">
+      <div className="flex justify-between w-full px-1.5">
+        <h2 className="text-lg font-semibold flex">📦 Ачаа</h2>
+        <div>
+          <p
+            className="text-sm text-gray-500 flex items-center"
+            onClick={() => router.push(`/goodsForUsers/${id}`)}
+          >
+            Order Status : {description}
+            <span className="text-gray-400 text-xl ml-2">›</span>
+          </p>
+       
+                    {showTrash && (
+      <AlertDialog>
+      <AlertDialogTrigger className="w-full flex justify-end mt-2">
+        <Trash2
+          stroke="red"
+          className="p-[4px] h-8 w-8 border rounded-full bg-gray-200"
+        />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Та үүнийг устгахдаа итгэлтэй байна уу?</AlertDialogTitle>
+          <AlertDialogDescription>
+          Ачааны дугаар: {trackingNumber}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Үгүй</AlertDialogCancel>
+          <AlertDialogAction onClick={() => DeleteTruckingByTrackingNumber(trackingNumber)}>
+            Тиймээ
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    
+           )}
         </div>
+        
       </div>
+
       <p>Ачааны дугаар: {trackingNumber}</p>
+
       <div className="flex gap-4">
         <button
-          className="mt-3 h-16 px-2 py-2 bg-gray-400 text-white rounded-lg w-full cursor-pointer hover:bg-[#5F2DF590]"
+          className="mt-3 text-[80%] h-10 px-2 py-2 bg-gray-200 text-black rounded-lg w-full cursor-pointer hover:bg-gray-500"
           onClick={() => router.push("/contact")}
         >
           Салбараас авах
         </button>
-        <button className="mt-3 h-16 px-2 py-2 bg-gray-400 text-white rounded-lg w-full cursor-pointer hover:bg-[#5F2DF590]">
+        <button className="mt-3 text-[80%] h-10 px-2 py-2 bg-gray-200 text-black rounded-lg w-full cursor-pointer hover:bg-gray-500">
           🚚 Хүргүүлэх
         </button>
       </div>
