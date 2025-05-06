f44f915964bbea7bac280a26dfe5e8c3e8d364ff
24LP7027 24LP7027 <24LP7027@G301-CC0211.local>
2025-05-06 16:59:17 +0800

    header adjustment

--- frontend/src/app/_location/Location.tsx
+++ frontend/src/app/_location/Location.tsx
@@ -16,7 +16,7 @@ const Location = ({ setStep }: { setStep: (value: string) => void }) => {
   const number = 1;
   const router = useRouter();
   return (
-    <div className="max-w-2xl w-full h-full overflow-scroll bg-[#e9ecef]">
+    <div className="max-w-2xl w-full h-full bt-[100px] overflow-scroll bg-[#e9ecef]">
       <div className="p-4">
         <Button
           className="w-full h-12 bg-white text-black justify-between rounded-lg flex font-bold"
