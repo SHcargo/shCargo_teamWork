d817ccc8f967c250e9e5f827632aecbe8a3b4372
tvkeprof <wefousoul@gmail.com>
2025-05-14 16:17:17 +0800

    g

--- frontend/src/app/_location/Location.tsx
+++ frontend/src/app/_location/Location.tsx
@@ -1,63 +1,66 @@
 "use client";
-import { Lightbulb, LocationEdit, MapPinHouse, Paperclip } from "lucide-react";
+import { Lightbulb } from "lucide-react";
 import InfoCard from "../components/card2";
-import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+
 import { useRouter } from "next/navigation";
 
-const Location = ({ setStep }: { setStep: (value: string) => void }) => {
+const Location = ({}: { setStep: (value: string) => void }) => {
   const number = 1;
   const router = useRouter();
   return (
     <div className="max-w-2xl w-full h-screen overflow-scroll bg-[#e9ecef]">
-      <div className="p-4">
-        <Button
-          className="w-full h-12 bg-white text-black justify-between hover:bg-black hover:text-white rounded-lg flex font-bold"
-          onClick={() => router.push(`/address/${number}`)}
-        >
-          Эрээн агуулах
-          <ChevronRight />
-        </Button>
+      <div>
+        <p className="font-bold p-4 text-2xl">Агуулах холбох</p>
+
+        <div className="grid grid-cols-2 gap-6 w-full p-4">
+          <InfoCard
+            icon={Lightbulb}
+            title="Эрээн агуулах"
+            subtitle="Жижиг том бүх бараа хүлээж авна"
+            onClick={() => router.push(`/address/${number}`)}
+          />
+
+          <InfoCard
+            icon={Lightbulb}
+            title="Хэбэй муж Шижяжуан хотын агуулах"
+            subtitle="Жижиг том бүх бараа хүлээж авна"
+            onClick={() => router.push(`/address/${number}`)}
+          />
+        </div>
       </div>
-      <div className="p-4 space-y-3">
-        <p className="font-bold">Тусламж</p>
-        <Button
-          className="w-full h-fit bg-white text-black hover:bg-black hover:text-white rounded-lg flex items-center font-bold px-4 py-3 text-left"
-          onClick={() => router.push(`/help/${number}`)}
-        >
-          <div className="flex w-full items-center justify-between flex-wrap">
-            <p className="text-base break-words whitespace-normal">
-              Хятад хаяг холбох болон Трак бүртгүүлэх заавар бичлэг үзэх
-            </p>
-            <ChevronRight className="flex-shrink-0 ml-2" />
-          </div>
-        </Button>
-      </div>
-      <div className="grid grid-cols-3 gap-6 w-full p-4">
-        <InfoCard
-          icon={LocationEdit}
-          title="Хаяг Холбох"
-          subtitle="Хятад дахь салбар"
-          onClick={() => setStep("profile")}
-        />
-        <InfoCard
-          icon={MapPinHouse}
-          title="SH Cargo Салбар"
-          subtitle="УБ дахь салбар"
-          onClick={() => router.push("/contact")}
-        />
+      <div className="grid grid-cols-2 gap-6 w-full p-4">
         <InfoCard
           icon={Lightbulb}
-          title="Ашиглах Заавар"
-          subtitle="Заавар, тайлбар"
-          onClick={() => router.push("/guide")}
+          title="Гуанжоу агуулах"
+          subtitle="Зөвхөн том 1м3-с дээш бараа хүлээж авна"
+          onClick={() => router.push(`/address/${number}`)}
         />
+
         <InfoCard
-          icon={Paperclip}
-          title="Үйлчилгээ Нөхцөл"
-          subtitle="SH Cargo журам"
-          onClick={() => router.push("/terms-conditions")}
+          icon={Lightbulb}
+          title="Шандун муж Линьи хот агуулах"
+          subtitle="Зөвхөн том 1м3-с дээш бараа хүлээж авна"
+          onClick={() => router.push(`/address/${number}`)}
         />
+      </div>
+
+      <div>
+        <p className="font-bold p-4 text-2xl">Тусламж</p>
+        <div className="grid grid-cols-2 gap-6 w-full p-4">
+          <InfoCard
+            icon={Lightbulb}
+            title="Ашиглах Заавар"
+            subtitle="Заавар, тайлбар"
+            onClick={() => router.push("/guide")}
+          />
+
+          <InfoCard
+            icon={Lightbulb}
+            title="Үйлчилгээ Нөхцөл"
+            subtitle="SH Cargo журам"
+            onClick={() => router.push("/terms-conditions")}
+          />
+        </div>
       </div>
     </div>
   );
