2f8f63669cb4a58b829f7e8d5ed2d09b01a37538
tvkeprof <wefousoul@gmail.com>
2025-05-15 16:15:20 +0800

    h

--- backend/src/router/help.router.ts
+++ backend/src/router/help.router.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import CreateHelpCenterController from "../controller/help/CreateHelpCenterController";
+import CreateHelpCenterController from "../controller/help/CreateHelpCenter.Controller";
 import GetHelpCenterController from "../controller/help/GetHelp.controller";
 
 export const HelpRouter = express.Router();
 HelpRouter.post("/salbar", CreateHelpCenterController);
-HelpRouter.delete("/salbar");
-HelpRouter.put("/salbar");
+// HelpRouter.delete("/salbar");
+// HelpRouter.put("/salbar");
 HelpRouter.get("/salbar", GetHelpCenterController);
